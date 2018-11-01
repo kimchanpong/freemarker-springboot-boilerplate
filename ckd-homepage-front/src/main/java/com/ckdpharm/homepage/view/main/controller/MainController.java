6bf32e71f197433bce2bffa031bab6738c5b3e72
ChanPong <rlacksgh0826@gmail.com>
2018-11-01 17:01:57 +0900

    add : freemarker layout 적용

--- ckd-homepage-front/src/main/java/com/ckdpharm/homepage/view/main/controller/MainController.java
+++ ckd-homepage-front/src/main/java/com/ckdpharm/homepage/view/main/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.ckdpharm.homepage.view.main.controller;
 
+import com.ckdpharm.homepage.common.view.ModuleAndView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,13 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 public class MainController {
 
 	@GetMapping(value = "/main")
-	public String main(Model model) {
-		return "main/main";
+	public ModelAndView main(ModuleAndView model) {
+		model.setTemplate("main/main");
+		return model.render();
 	}
 }
--- ckd-homepage-front/src/main/resources/templates/layouts/default/layout.ftl
+++ ckd-homepage-front/src/main/resources/templates/layouts/default/layout.ftl
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
-    <title>테스트</title>
+    <title>default</title>
 </head>
 <body>
-    테스트 내용123
+    <#include body>
 </body>
 </html>
