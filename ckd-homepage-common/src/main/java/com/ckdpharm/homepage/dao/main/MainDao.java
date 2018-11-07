81f6577c0c4c88366911f43d9a616b285cc393b4
ChanPong <rlacksgh0826@gmail.com>
2018-11-07 15:31:32 +0900

    modify : JOOQ 테이블 도메인 사용 방식 변경

--- ckd-homepage-common/src/main/java/com/ckdpharm/homepage/dao/main/MainDao.java
+++ ckd-homepage-common/src/main/java/com/ckdpharm/homepage/dao/main/MainDao.java
@@ -4,33 +4,34 @@ import com.ckdpharm.homepage.dao.common.BaseDao;
 import com.ckdpharm.homepage.model.main.MainPO;
 import com.ckdpharm.homepage.model.main.MainSO;
 import com.ckdpharm.homepage.model.main.MainVO;
-import com.ckdpharm.jooq.domain.tables.Author;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
+import static com.ckdpharm.jooq.domain.tables.Author.AUTHOR;
+
 @Repository
 public class MainDao extends BaseDao {
-    private Author author = Author.AUTHOR;
 
     public List<MainVO> selectList() {
         return dao
-                .selectFrom(author)
+                .selectFrom(AUTHOR)
                 .fetchInto(MainVO.class);
     }
 
     public MainVO selectVo(MainSO so) {
         return dao
                 .select()
-                .from(author)
-                .where(author.ID.eq(so.getId()))
+                .from(AUTHOR)
+                .where(AUTHOR.ID.eq(so.getId()))
                 .fetchAny().into(MainVO.class);
     }
 
     public int insertVo(MainPO po) {
         /** insertInto의 첫번째 인자는 무조건 테이블 domain이 와야한다 */
         return dao
-                .insertInto(author, author.ID, author.FIRST_NAME, author.LAST_NAME)
+                .insertInto(AUTHOR, AUTHOR.ID, AUTHOR.FIRST_NAME, AUTHOR.LAST_NAME)
                 .values(po.getId(), po.getFirstName(), po.getLastName())
                 .execute();
     }
@@ -38,10 +39,10 @@ public class MainDao extends BaseDao {
     public int updateVo(MainPO po) {
         /** update의 첫번째 인자는 무조건 테이블 domain이 와야한다 */
         return dao
-                .update(author)
-                .set(author.FIRST_NAME, po.getFirstName())
-                .set(author.LAST_NAME, po.getLastName())
-                .where(author.ID.eq(po.getId()))
+                .update(AUTHOR)
+                .set(AUTHOR.FIRST_NAME, po.getFirstName())
+                .set(AUTHOR.LAST_NAME, po.getLastName())
+                .where(AUTHOR.ID.eq(po.getId()))
                 .execute()
                 ;
     }
