0a8887d6198e2205b613c232dcf7a95a0153fd6a
ChanPong <rlacksgh0826@gmail.com>
2018-11-07 16:41:50 +0900

    modify : 변수 위치 변경

--- ckd-homepage-common/src/main/java/com/ckdpharm/homepage/common/view/ModuleAndView.java
+++ ckd-homepage-common/src/main/java/com/ckdpharm/homepage/common/view/ModuleAndView.java
@@ -7,8 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 
 public class ModuleAndView extends ModelAndView {
     /**
-     * 레이아웃을 메인 템플릿으로 사용하여 body 속에 템플릿을 include 하는 방식으로 페이지를 완성한다.
+     *  레이아웃을 메인 템플릿으로 사용하여 body 속에 템플릿을 include 하는 방식으로 페이지를 완성한다.
+     *  ----------------------------------------------------------------------------------
+     *  freemarker는 include가 삽입되는 파일의 디렉토리 경로를 기본으로 두고있다.
+     *  예를들어 footer.ftl을 layout.ftl에 include할때 layouts디렉토리가 자동으로 잡히기때문에
+     *  layouts이후의 디렉토리명부터 기술한다
      */
+    HttpServletRequest request = HttpUtil.getHttpServletRequest();
+    String devicePath = request.getRequestURI().startsWith("/m/") ? "mobile" : "pc";
+    String style = "include/style.ftl";
+    String footer = "include/footer.ftl";
+    String script = "include/script.ftl";
+    String layout = devicePath + "/layouts/layout";
 
     // 템플릿
     private String template;
@@ -26,18 +36,6 @@ public class ModuleAndView extends ModelAndView {
     }
 
     public ModelAndView render(boolean showLayout) {
-        HttpServletRequest request = HttpUtil.getHttpServletRequest();
-        String devicePath = request.getRequestURI().startsWith("/m/") ? "mobile" : "pc";
-
-        /**
-            freemarker는 include가 삽입되는 파일의 디렉토리 경로를 기본으로 두고있다.
-            예를들어 footer.ftl을 layout.ftl에 include할때 layouts디렉토리가 자동으로 잡히기때문에
-            layouts이후이 디렉토리명부터 기술한다
-         */
-        String style = "include/style.ftl";
-        String footer = "include/footer.ftl";
-        String script = "include/script.ftl";
-        String layout = devicePath + "/layouts/layout";
         String body = "/" + devicePath + "/modules/views/" + this.template + ".ftl";
 
         if (!showLayout) {
